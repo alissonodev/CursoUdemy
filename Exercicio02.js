7ff493e0971ce4fdc595f4bc6100b3501eb8b11a
Alisson Cardoso <alisson.fcardoso15@gmail.com>
2026-03-19 14:24:14 -0300

    Exercício 03 (Logica de programação)
    1. O Operador de Resto (%)
    Como você bem anotou no comentário, o % chama-se módulo. Ele não retorna o resultado da divisão, mas sim o que sobra dela.
    
    6 % 3 é 0 (porque 6 dividido por 3 é 2 e não sobra nada).
    
    7 % 3 é 1 (porque 7 dividido por 3 dá 2 e sobra 1).
    
    A lógica: Se numero % x === 0, significa que o número é divisível por x.
    
    2. A Ordem das Condições (Muito Importante!)
    Repare que a primeira linha da sua função checa os dois ao mesmo tempo:
    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
    
    Por que ela vem primeiro? Porque se você checasse apenas o 3 primeiro, o número 15 (que é divisível por 3 e 5) entraria no primeiro if, retornaria 'Fizz' e sairia da função sem nunca chegar no 'FizzBuzz'. Na programação, a condição mais específica deve vir antes da mais genérica.
    
    3. Os Retornos
    return 'Fizz': Se for divisível apenas por 3.
    
    return 'Buzz': Se for divisível apenas por 5.
    
    return NaN: Se o número não passar em nenhuma das regras acima, a função retorna "Not a Number".
    
    Explicação do Laço for
    
    let i = 0: O contador começa em zero.
    
    i <= 100: O loop vai rodar enquanto o i for menor ou igual a 100.
    
    i++: A cada volta, adiciona 1 ao valor de i.
    
    console.log(i, fizzBuzz(i)): Em cada volta, ele imprime o número atual e chama a função para saber o que deve escrever ao lado dele.

--- Exercicio02.js
+++ Exercicio02.js
@@ -1,9 +1,9 @@
 function ePaisagem(largura, altura) {
-    return largura > altura; //se colocar um >= ele retorna "true" caso a imagem for quadrada.
+    return largura > altura; //Se colocar um >= ele retorna "true" caso a imagem for quadrada.
 }
 
 console.log(ePaisagem(1080, 1920))
 
-//rescrevi o código
+//Rescrevi o código
 const ePaisagem2 = (largura, altura) => largura >= altura;
 console.log(ePaisagem2(1920, 1920))
